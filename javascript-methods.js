8e8843ccb5ef75f1e04d7a2483d1f9c9a67cd3ee
remit233 <joeytan233@gmail.com>
2022-06-09 17:35:01 -0400

    Map, reduce function done

--- javascript-methods.js
+++ javascript-methods.js
@@ -1,7 +1,17 @@
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
   // Place your code here.
+  let  temp = [];
+  for (let i = 0; i < this.length; i++){
+    temp.push(callbackFn(this[i], i, this));
+  }
+  return temp;
 };
+/*testing
+const array1 = [1,2,3];
+const map1 = array1.myMap((x) => x*6);
+console.log(map1)
+*/
 
 // FILTER //
 Array.prototype.myFilter = function(callbackFn) {
@@ -21,7 +31,24 @@ Array.prototype.myEvery = function(callbackFn) {
 // REDUCE //
 Array.prototype.myReduce = function(callbackFn) {
   // Place your code here.
+  let temp1 = [];
+  let temp = initialValue;
+  for (let i = 1; i < this.length; i++){
+    temp = callbackFn(this[i-1], this[i]);
+    this[i] = temp;
+  }
+  return temp;
 };
+/* testing
+const array1 = [1, 2, 3, 4];
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.myReduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+console.log(sumWithInitial);
+*/
 
 // INCLUDES //
 Array.prototype.myIncludes = function(searchElement) {
